ca1d98402f7584de2bba34d51e5ab642e8c6e1e4
Murray Christopherson <murraychristopherson@gmail.com>
2020-04-23 22:23:59 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+To see issue:
+
+In project root:
+
+`yarn global add @angular/cli`
+
+`yarn install --frozen-lockfile`
+
+`ng e2e`
---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+﻿import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+
+import { routes } from '@app/app.routing';
+import { AppComponent } from '@app/app.component';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+
+    RouterModule.forRoot(routes),
+  ],
+  declarations: [
+    AppComponent,
+  ],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
